f42c939def10b6b0a7e3edabe1e8652bb06b7747
wudang <dang.wu@melot.cn>
2024-01-15 17:31:44 +0800

    deploy

--- lib/main.dart
+++ lib/main.dart
@@ -23,3 +23,14 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+/*
+部署
+1.拷贝.ttf .otf到 build/web/canvaskit/
+2.修改index.html   <base href="/res/web/">
+3.修改main.dart.js
+- m="http://10.0.6.191/res/web/"+j
+- canvaskit/KFOmCnqEu92Fr1Me5WZLCzYlKw.ttf
+- canvaskit/k3kXo84MPvpLmixcA63oeALhL4iJ-Q7m8w.otf
+ */
